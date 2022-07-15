16f4064eb1c580e2a45869b07432440e1c241603
Bernard Kissi <bernardkissi>
2022-07-15 12:35:24 +0200

    updated dependencies

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,12 +1,13 @@
 import { createSSRApp, h } from 'vue'
 import { renderToString } from '@vue/server-renderer'
 import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import createServer from '@inertiajs/server'
 
 createServer((page) => createInertiaApp({
   page,
   render: renderToString,
-  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),,
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ app, props, plugin }) {
     return createSSRApp({
       render: () => h(app, props),
