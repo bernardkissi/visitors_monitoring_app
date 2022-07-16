a4ac40b999c8c7e22172b66da03173fda20c854b
Bernard Kissi <bernardkissi>
2022-07-16 11:48:28 +0200

    updated visitors create form

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gracefields Visitors System</title>
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
